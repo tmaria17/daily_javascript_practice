f83d36f09bb3db39abf230133a8df03de2039fab
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-03-19 11:20:04 -0600

    Vampire complete. All tests passing.

---
+++ mythical-creatures/exercises/vampire.js
@@ -0,0 +1,14 @@
+class Vampire {
+  constructor(name, pet='bat'){
+    this.name = name;
+    this.pet = pet;
+    this.thirsty = true;
+
+  }
+
+  drink(){
+    this.thirsty = false;
+  }
+}
+
+module.exports = Vampire;
