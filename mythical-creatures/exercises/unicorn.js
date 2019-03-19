da91cf5acc31a09ada5ced8882321c267548519a
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-03-19 11:05:30 -0600

    Unicorn done. All tests passing.

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,6 +1,17 @@
 class Unicorn {
-  constructor() {
+  constructor(name, color='white') {
+    this.name = name;
+    this.color = color;
   }
+
+  isWhite(){
+    return (this.color == 'white')
+  }
+
+  says(string){
+    return '**;* ' + string + ' *;**'
+  }
+
 }
 
 module.exports = Unicorn;
